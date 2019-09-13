0cf430506a75ae349e32ca589619eb7eee43033a
willianmarquesfsa <willianmarques_fsa@hotmail.com>
2019-09-13 11:06:18 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class ComprasApp extends StatelessWidget{
 final routes = <String, WidgetBuilder> {
   HomePage.tag: (context) => HomePage()
 };
-
+//teste
 @override
 Widget build(BuildContext context) {
 
@@ -17,7 +17,7 @@ return MaterialApp(
        title: "ComprasApp",
        theme: ThemeData(
               primaryColorDark: Colors.blueGrey,
-              accentColor: Colors.grey,
+              accentColor: Colors.grey[200],
               textTheme: TextTheme(
                          headline: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
                          title: TextStyle(fontSize: 36, fontStyle: FontStyle.italic, color: Colors.blueGrey),
