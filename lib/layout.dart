92c97c4c0c1afc5d597e85c031e51f421cd78ba1
willianmarquesfsa <willianmarques_fsa@hotmail.com>
2019-09-21 20:30:06 -0300

    Versao.1.222

--- lib/layout.dart
+++ lib/layout.dart
@@ -62,6 +62,22 @@ return Scaffold(appBar:AppBar(
 
               );
 
-}
+
+
+
+
 
 }
+
+
+static Color primary([double opacity = 1 ]) => Color.fromRGBO(62, 63, 89, opacity);
+static Color secundary([double opacity = 1 ]) => Color.fromRGBO(111, 168, 191, opacity);
+static Color ligth([double opacity = 1 ]) => Color.fromRGBO(242, 234, 228, opacity);
+static Color dark([double opacity = 1 ]) => Color.fromRGBO(51, 51, 51, opacity);
+static Color danger([double opacity = 1 ]) => Color.fromRGBO(217, 74, 74, opacity);
+static Color success([double opacity = 1 ]) => Color.fromRGBO(6, 166, 59, opacity);
+static Color info([double opacity = 1 ]) => Color.fromRGBO(0, 122, 166, opacity);
+static Color warning([double opacity = 1 ]) => Color.fromRGBO(166, 134, 0, opacity);
+
+}
+
